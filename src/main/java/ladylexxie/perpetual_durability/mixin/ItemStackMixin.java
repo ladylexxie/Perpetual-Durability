4b8aed4b48b1bfa83f650f81fd21e8990921f9f5
Lady Lexxie <merideo041096@gmail.com>
2022-06-08 09:45:21 +0100

    mixin fix

--- src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
+++ src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
@@ -4,6 +4,7 @@ import ladylexxie.perpetual_durability.registry.LexRegistry;
 import ladylexxie.perpetual_durability.util.ModdedEnchantmentHelper;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,13 +15,21 @@ import java.util.Random;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
-    @Shadow
+    @Shadow(remap = false)
     public abstract ItemStack copy();
 
-    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true)
-    private void handleIndestructibleEnchantment(int damage, Random rand, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> info){
+    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true, remap = false)
+    private void handlePerpetualEnchantment(int damage, Random rand, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> info){
         if(ModdedEnchantmentHelper.hasEnchant(this.copy(), LexRegistry.PERPETUAL.get())){
+            LogManager.getLogger().info("coite");
             info.setReturnValue(false);
         }
     }
+
+//    @Inject(at = @At(value = "HEAD"), method = "isDamageableItem", cancellable = true)
+//    private void handlePerpetualSomethingEnchantment(CallbackInfoReturnable<Boolean> info){
+//        if(ModdedEnchantmentHelper.hasEnchant(this.copy(), LexRegistry.PERPETUAL.get())){
+//            info.setReturnValue(false);
+//        }
+//    }
 }
--- src/main/java/ladylexxie/perpetual_durability/util/ModdedEnchantmentHelper.java
+++ src/main/java/ladylexxie/perpetual_durability/util/ModdedEnchantmentHelper.java
@@ -3,12 +3,17 @@ package ladylexxie.perpetual_durability.util;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
+import org.apache.logging.log4j.LogManager;
 
 public class ModdedEnchantmentHelper {
-    public static boolean hasEnchant(ItemStack item, Enchantment enchantment){
-        for(Enchantment itemEnchantment : EnchantmentHelper.getEnchantments(item).keySet()){
-            if(itemEnchantment == enchantment) return true;
+    public static boolean hasEnchant(ItemStack item, Enchantment enchantment) {
+        for (Enchantment itemEnchantment : EnchantmentHelper.getEnchantments(item).keySet()) {
+            if (itemEnchantment == enchantment) {
+                LogManager.getLogger().info("true");
+                return true;
+            }
         }
+        LogManager.getLogger().info("false");
         return false;
     }
 }
