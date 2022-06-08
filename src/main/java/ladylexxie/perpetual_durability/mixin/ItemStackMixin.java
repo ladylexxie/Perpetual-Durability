3ac222a21c414e445593546671b06a8c19ba9dc1
Lady Lexxie <merideo041096@gmail.com>
2022-06-08 08:52:32 +0100

    ewqe

--- src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
+++ src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
@@ -2,7 +2,6 @@ package ladylexxie.perpetual_durability.mixin;
 
 import ladylexxie.perpetual_durability.registry.LexRegistry;
 import ladylexxie.perpetual_durability.util.ModdedEnchantmentHelper;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,17 +9,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Random;
-
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
     @Shadow
     public abstract ItemStack copy();
 
-    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true)
-    private void handleIndestructibleEnchantment(int damage, Random rand, ServerPlayer player, CallbackInfoReturnable<Boolean> info){
+    @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;getTag()Lnet/minecraft/nbt/CompoundTag;"), method = "isDamageableItem", cancellable = true)
+    private void handlePerpetualEnchantment(CallbackInfoReturnable<Boolean> cir) {
         if(ModdedEnchantmentHelper.hasEnchant(this.copy(), LexRegistry.PERPETUAL.get())){
-            info.setReturnValue(false);
+            cir.setReturnValue(false);
         }
     }
 }
