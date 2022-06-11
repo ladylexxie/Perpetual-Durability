30ee05c22f93b2b4eeb30e0a993f81ea457a097e
Lady Lexxie <merideo041096@gmail.com>
2022-06-11 12:47:36 +0100

    ewgte

--- src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
+++ src/main/java/ladylexxie/perpetual_durability/mixin/ItemStackMixin.java
@@ -4,7 +4,6 @@ import ladylexxie.perpetual_durability.registry.LexRegistry;
 import ladylexxie.perpetual_durability.util.ModdedEnchantmentHelper;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
-import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +14,10 @@ import java.util.Random;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
-    @Shadow(remap = false)
+    @Shadow
     public abstract ItemStack copy();
 
-    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true, remap = false)
+    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true)
     private void handlePerpetualEnchantment(int damage, Random rand, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> info){
         if(ModdedEnchantmentHelper.hasEnchant(this.copy(), LexRegistry.PERPETUAL.get())){
             info.setReturnValue(false);
