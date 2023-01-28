b42433edfb9543fdea1b69ed34c44d6acf9150cd
Lexxie <merideo041096@gmail.com>
2023-01-28 15:07:17 +0000

    what

--- src/main/java/ladylexxie/perpetual_durability/recipe/ApplyPerpetual.java
+++ src/main/java/ladylexxie/perpetual_durability/recipe/ApplyPerpetual.java
@@ -1,16 +1,16 @@
 package ladylexxie.perpetual_durability.recipe;
 
+import com.google.gson.JsonObject;
 import ladylexxie.perpetual_durability.PerpetualDurability;
 import ladylexxie.perpetual_durability.config.EnchantConfig;
 import ladylexxie.perpetual_durability.registry.LexRegistry;
 import ladylexxie.perpetual_durability.util.ModdedEnchantmentHelper;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.UpgradeRecipe;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
@@ -19,11 +19,17 @@ import java.util.List;
 import java.util.Objects;
 
 public class ApplyPerpetual extends UpgradeRecipe {
+	final Ingredient base;
+	final Ingredient addition;
+	final ItemStack result;
 	private final List<String> modBlacklist = (List<String>) EnchantConfig.MOD_BLACKLIST.get();
 	private final List<String> itemBlacklist = (List<String>) EnchantConfig.ITEM_BLACKLIST.get();
 
 	public ApplyPerpetual( ResourceLocation recipeID ) {
 		super(recipeID, Ingredient.EMPTY, Ingredient.EMPTY, ItemStack.EMPTY);
+		this.base = Ingredient.EMPTY;
+		this.addition = Ingredient.EMPTY;
+		this.result = ItemStack.EMPTY;
 	}
 
 	@Override
@@ -62,5 +68,27 @@ public class ApplyPerpetual extends UpgradeRecipe {
 	@Override
 	public @NotNull RecipeSerializer<?> getSerializer() {
 		return LexRegistry.APPLY_PERPETUAL.get();
+	}
+
+	public static class Serializer implements RecipeSerializer<ApplyPerpetual> {
+		public ApplyPerpetual fromJson(ResourceLocation p_44562_, JsonObject p_44563_) {
+			Ingredient ingredient = Ingredient.fromJson(GsonHelper.getAsJsonObject(p_44563_, "base"));
+			Ingredient ingredient1 = Ingredient.fromJson(GsonHelper.getAsJsonObject(p_44563_, "addition"));
+			ItemStack itemstack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(p_44563_, "result"));
+			return new ApplyPerpetual(p_44562_);
+		}
+
+		public ApplyPerpetual fromNetwork(ResourceLocation p_44565_, FriendlyByteBuf p_44566_) {
+			Ingredient ingredient = Ingredient.fromNetwork(p_44566_);
+			Ingredient ingredient1 = Ingredient.fromNetwork(p_44566_);
+			ItemStack itemstack = p_44566_.readItem();
+			return new ApplyPerpetual(p_44565_);
+		}
+
+		public void toNetwork(FriendlyByteBuf p_44553_, ApplyPerpetual p_44554_) {
+			p_44554_.base.toNetwork(p_44553_);
+			p_44554_.addition.toNetwork(p_44553_);
+			p_44553_.writeItem(p_44554_.result);
+		}
 	}
 }
