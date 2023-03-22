295273b8eaad65bdcffe201db9567f4184cb1a9d
Lexxie <merideo041096@gmail.com>
2023-03-22 22:24:15 +0000

    Eh..

--- forge/src/main/java/ladylexxie/perpetual_durability/rei/PDREIPluginForge.java
+++ forge/src/main/java/ladylexxie/perpetual_durability/rei/PDREIPluginForge.java
@@ -1,4 +1,4 @@
-package ladylexxie.perpetual_durability;
+package ladylexxie.perpetual_durability.rei;
 
 import ladylexxie.perpetual_durability.compat.PDREIPlugin;
 import me.shedaniel.rei.forge.REIPluginClient;
