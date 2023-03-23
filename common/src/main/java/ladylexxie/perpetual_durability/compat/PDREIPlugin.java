3200db184e6be7463a1a7e54ce13bd7e0597ca93
Lexxie <merideo041096@gmail.com>
2023-03-23 23:37:19 +0000

    More Ehhhhh

--- common/src/main/java/ladylexxie/perpetual_durability/compat/PDREIPlugin.java
+++ common/src/main/java/ladylexxie/perpetual_durability/compat/PDREIPlugin.java
@@ -3,18 +3,20 @@ package ladylexxie.perpetual_durability.compat;
 import ladylexxie.perpetual_durability.recipe.PerpetuateRecipe;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.plugin.client.BuiltinClientPlugin;
 import me.shedaniel.rei.plugin.common.displays.DefaultSmithingDisplay;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.UpgradeRecipe;
+
+import java.util.List;
 
 public class PDREIPlugin implements REIClientPlugin {
 	@Override
 	public void registerDisplays( DisplayRegistry registry ) {
-//		List<UpgradeRecipe> recipes = CompatHelper.getPerpetuateRecipes(CompatHelper.XEI.REI);
-//		for( UpgradeRecipe recipe : recipes ) {
-			//			registry.get(BuiltinClientPlugin.SMITHING).add(new DefaultSmithingDisplay(recipe));
-//			registry.registerRecipeFiller(PerpetuateRecipe.class, RecipeType.SMITHING, () -> new DefaultSmithingDisplay(recipe));
-//		}
-			registry.registerRecipeFiller(PerpetuateRecipe.class, RecipeType.SMITHING, DefaultSmithingDisplay::new);
-
+		List<UpgradeRecipe> recipes = CompatHelper.getPerpetuateRecipes(CompatHelper.XEI.REI);
+		for( UpgradeRecipe recipe : recipes ) {
+			registry.get(BuiltinClientPlugin.SMITHING).add(new DefaultSmithingDisplay(recipe));
+			registry.registerRecipeFiller(PerpetuateRecipe.class, RecipeType.SMITHING, () -> new DefaultSmithingDisplay(recipe));
+		}
 	}
 }
